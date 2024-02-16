5d4fa6786f9e3f21d026f5f6c0887925168686f3
Victor Oliveira <vggaia10@gmail.com>
2024-02-16 11:28:37 -0300

    feat: add select

---
+++ components/Select/index.tsx
@@ -0,0 +1,43 @@
+import { CSSProperties, ChangeEvent, ReactNode } from 'react'
+
+
+import * as S from './styles'
+
+interface ISelect {
+  styled?: string
+  style?: CSSProperties
+  erro?: boolean
+  label?: string
+  id: string
+  disabled?: boolean
+  name?: string
+  value: any
+  onChange: (event: ChangeEvent<HTMLSelectElement>) => void
+  children: ReactNode
+  className?: string
+}
+
+export default function Select({
+  id,
+  disabled,
+  name = id,
+  value,
+  onChange,
+  children,
+  className
+}: ISelect) {
+  return (
+
+    <S.SelectStyled
+      disabled={disabled}
+      id={id}
+      name={name}
+      className={className}
+      value={value}
+      onChange={onChange}
+    >
+      {children}
+    </S.SelectStyled>
+
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Button from '@/components/Button';
+import Select from '@/components/Select';
 
 
 export default function Home() {
+
+  const [ selected, setSelected ] = useState('')
+
   return (
     <>
-      a
+      <Button>a</Button>
+
+      <Select id='a' onChange={(e) => setSelected(e.target.value)} value={selected}>
+        <option>a</option>
+        <option>b</option>
+        <option>c</option>
+      </Select>
     </>
   );
 }
