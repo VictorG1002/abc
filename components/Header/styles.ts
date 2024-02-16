901058f1307efedf03d9efa8e055a1bd66fb7713
Victor Oliveira <vggaia10@gmail.com>
2024-02-16 10:05:31 -0300

    fix: format header code

--- components/Header/styles.ts
+++ components/Header/styles.ts
@@ -4,12 +4,11 @@ export const HeaderContainer = styled.div`
   padding: 40px;
   width: 100%;
   background-color: #63b3ed;
-  box-shadow: 10px 10px 88px 0px rgba(0,0,0,0.2);
-
+  box-shadow: 10px 10px 88px 0px rgba(0, 0, 0, 0.2);
 
   & div {
     color: white;
     font-size: 24px;
-    font-family: 'Poppins'
+    font-family: "Poppins";
   }
 `;
