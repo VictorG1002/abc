67f197e9755826e5bcb561a74fbcc71bba03eb49
Victor Oliveira <vggaia10@gmail.com>
2024-02-16 10:05:13 -0300

    feat: create footer

---
+++ components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import * as S from './styles'
+
+
+
+export default function Footer() {
+  return (
+    <S.FooterContainer>
+      <div>suporte: vggaia@gmail.com</div>
+      <div>suporte: vggaia@gmail.com</div>
+      <div>suporte: vggaia@gmail.com</div>
+    </S.FooterContainer>
+  )
+}
